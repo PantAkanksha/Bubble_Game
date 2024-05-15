1bbe8fb909fb68e070ea401255817d636ea93f4f
PantAkanksha <akankshpant@gmail.com>
2024-05-15 23:29:07 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var timer = 60;
+var hitrn = 0;
+var score = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreVal").textContent = score;
+}
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent = hitrn;
+}
+getNewHit();
+function runTimer(){
+    let timerInt = setInterval(function() {
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timerVal").textContent = timer;
+        }
+        else{
+            clearInterval(timerInt)
+            document.querySelector("#pbtm").innerHTML = "<h1>GAME OVER!</h1>";
+        }
+    }, 1000);
+}
+runTimer();
+
+function makeBubble(){
+    var clutter = "";
+    for(var i = 1; i <= 133; i++){
+        var rn = Math.floor(Math.random()*10);
+        clutter += `<div class = "bubble">${rn}</div>`;
+    }
+    console.log("Generated HTML for bubbles:", clutter);
+    document.querySelector("#pbtm").innerHTML = clutter
+}
+makeBubble();
+document.querySelector("#pbtm").addEventListener("click", function(dets){
+    var clickedNum = (Number(dets.target.textContent));
+    if(clickedNum == hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+
